ec854624442efd63a985faf1a033a876da05b012
petershahlevlnow <pshah82@gmail.com>
2017-11-27 21:57:30 -0800

    Initial summary of data and structure

--- initSummary.R
+++ initSummary.R
@@ -1,6 +1,22 @@
 # Source getDetroitData
 source("getDetroitData.r") #run this to get data
 
-# intitial summary of data
+# intitial summary of data - 311 issues
+head(det311)
 str(det311)
 summary(det311)
+
+# initial summary of data - Crime
+head(detCrime1216_pres)
+str(detCrime1216_pres)
+summary(detCrime1216_pres)
+
+# initial summary of data - Demolitions
+head(detDemolitions)
+str(detDemolitions)
+summary(detDemolitions)
+
+# initial summary of data - Blight
+head(detBlight)
+str(detBlight)
+summary(detBlight)
