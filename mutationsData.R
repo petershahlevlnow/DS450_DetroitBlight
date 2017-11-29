1633c555c3ef926392a4aa9cd15f81c0de41430e
petershahlevlnow <pshah82@gmail.com>
2017-11-28 22:52:41 -0800

    group incident data by loc.id, create a unique list of location ids with lat and long

--- mutationsData.R
+++ mutationsData.R
@@ -26,3 +26,25 @@ d311 <- cbind(d311, a)
 a <- dummy(dCrime$crm.type, sep = "")
 colnames(a) <- gsub("crm.type", "", colnames(a), fixed = TRUE)
 dCrime <- cbind(dCrime, a)
+
+# group columns on location id and sum incidents
+temp.gr.311 <- d311[, 4:ncol(d311)] %>% group_by(loc.id) %>% summarise_all(funs(sum))
+temp.gr.Blight <- dBlight %>% group_by(loc.id) %>% count()
+temp.gr.Crime <- dCrime[, 4:ncol(dCrime)] %>% group_by(loc.id) %>% summarise_all(funs(sum))
+# No id grouping results from demo data, consider removing next line (i.e. same as dDemo)
+temp.gr.Demo <- dDemo %>% select(loc.id, d.price) %>% group_by(loc.id) %>% summarise_all(funs(sum))
+
+# create master list of unique loc.ids with lat and long from all dfs
+unique.loc <- d311 %>% select(loc.id, lat, long)
+unique.loc <- rbind(unique.loc, dBlight %>% select(loc.id, lat, long))
+unique.loc <- rbind(unique.loc, dCrime %>% select(loc.id, lat, long))
+unique.loc <- rbind(unique.loc, dDemo %>% select(loc.id, lat, long))
+unique.loc <- unique.loc %>% select(loc.id, lat, long) %>% group_by(loc.id, lat, long) %>% count()
+
+# join all temp dataset columns to unique.loc - master list of unique incident locations.
+
+# clean up final data
+
+# rename columns
+
+# remove uncategorized crime incidents (lat = 32.... long = -127)
