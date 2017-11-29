eb10b7a76f8689ec9663d71ca39e12ca864530b1
Peter Shah <peter.shah@levlnow.com>
2017-11-28 18:03:29 -0800

    Extend type factors and bind for counting

--- mutationsData.R
+++ mutationsData.R
@@ -12,3 +12,17 @@ d311$loc.id <- gsub("[^0-9]", "", paste(d311$lat, d311$long, sep = ""))
 dBlight$loc.id <- gsub("[^0-9]", "", paste(dBlight$lat, dBlight$long, sep = ""))
 dCrime$loc.id <- gsub("[^0-9]", "", paste(dCrime$lat, dCrime$long, sep = ""))
 dDemo$loc.id <- gsub("[^0-9]", "", paste(dDemo$lat, dDemo$long, sep = ""))
+
+# extend out factors to dummy variables for counting
+
+# d311 
+
+library(dummies)
+a <- dummy(d311$inc.type, sep = "")
+colnames(a) <- gsub("inc.type", "", colnames(a), fixed = TRUE)
+d311 <- cbind(d311, a)
+
+# dCrime
+a <- dummy(dCrime$crm.type, sep = "")
+colnames(a) <- gsub("crm.type", "", colnames(a), fixed = TRUE)
+dCrime <- cbind(dCrime, a)
