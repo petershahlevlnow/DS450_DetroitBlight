eec22e42b01ef0ff6ddce0af5b0d4319c78779c4
Peter Shah <peter.shah@levlnow.com>
2017-12-01 12:44:19 -0800

    contour and raster density plot

--- exploreData.R
+++ exploreData.R
@@ -7,6 +7,7 @@ source("mutationsData.R")
 library(ggplot2)
 library(ggmap)
 library(reshape2)
+library(gridExtra)
 
 #  citation: 
 #  D. Kahle and H. Wickham. ggmap: Spatial Visualization with ggplot2. The R Journal, 5(1), 144-161. URL
@@ -28,4 +29,40 @@ detMap.freq <- ggmap(detMap) + geom_point(data = sample_n(melt.detAll %>% group_
 
 detMap.freq
 
+# plot contour density on map
+
+detMap.den.crime <- ggmap(detMap) + stat_density2d(data = melt.detAll %>% filter(variable == "nCrime"), 
+                                         aes(x = long, y = lat), bins = 30)
+
+# plot raster 
+# set color ramp
+colfunc <- colorRampPalette(c("white", "lightblue", "green", "yellow", "red"))
+
+detMap.den.crime <- ggmap(detMap) + stat_density2d(data = sample_n(melt.detAll %>% filter(variable == "nCrime"), 5000), 
+                                                   aes(x = long, y = lat, fill = ..density..),  
+                                                   geom = "tile", contour = FALSE, alpha = 0.3) +
+                                    scale_fill_gradientn(colours=colfunc(400)) + ggtitle("Criminal Incidents 2016")
+
+detMap.den.311 <- ggmap(detMap) + stat_density2d(data = sample_n(melt.detAll %>% filter(variable == "n311"), 5000), 
+                                                 aes(x = long, y = lat, fill = ..density..),  
+                                                 geom = "tile", contour = FALSE, alpha = 0.3) +
+                                  scale_fill_gradientn(colours=colfunc(400)) + ggtitle("311 Incidents 2016")
+
+detMap.den.demo <- ggmap(detMap) + stat_density2d(data = sample_n(melt.detAll %>% filter(variable == "nDemo"), 5000), 
+                                                 aes(x = long, y = lat, fill = ..density..),  
+                                                 geom = "tile", contour = FALSE, alpha = 0.3) +
+                                    scale_fill_gradientn(colours=colfunc(400)) + ggtitle("Demolitions 2016")
+
+detMap.den.blight <- ggmap(detMap) + stat_density2d(data = sample_n(melt.detAll %>% filter(variable == "nBlight"), 5000), 
+                                                  aes(x = long, y = lat, fill = ..density..),  
+                                                  geom = "tile", contour = FALSE, alpha = 0.3) +
+                                   scale_fill_gradientn(colours=colfunc(400)) + ggtitle("Blight Violations YTD 2017")
+
+
+# detMap.den.crime
+# detMap.den.311
+# detMap.den.demo
+# detMap.den.blight
+
+grid.arrange(detMap.den.crime, detMap.den.311, detMap.den.demo, detMap.den.blight, nrow = 2, ncol = 2)
 rm(a)
